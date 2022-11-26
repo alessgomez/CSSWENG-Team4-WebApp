44d1940e989083dc3a812192765802bcfbd02739
Alessandra Gomez <94123165+alessgomez@users.noreply.github.com>
2022-11-27 02:30:10 +0800

    Removed pw validation

--- public/js/admin_login.js
+++ public/js/admin_login.js
@@ -4,24 +4,16 @@ $(document).ready(function(){
     var btn = document.getElementById("login");
 
     username.onkeyup = function () {
-        if (username.value.length != 0 && validatePW(pw))
+        if (username.value.length != 0 && pw.value.length != 0)
             btn.disabled = false;
         else
             btn.disabled = true;
     }
 
     pw.onkeyup = function () {
-        if (username.value.length != 0 && validatePW(pw))
+        if (username.value.length != 0 && pw.value.length != 0)
             btn.disabled = false;
         else
             btn.disabled = true;
-    }
-
-    function validatePW (pw) {
-        var uppercaseLetters = /[A-Z]/g;
-        var specialChars = /\W|_/g;
-        var numbers = /[0-9]/g;
-        
-        return pw.match(uppercaseLetters) && pw.match(specialChars) && pw.match(numbers) && pw.length >= 8;
     }
 });
